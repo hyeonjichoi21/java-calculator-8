9fc983682a77add2a6b6d37732b0ebc19ff01dca
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-10-20 18:28:30 +0900

    feat(calculator): 커스텀 구분자 덧셈 기능 및 에러처리 구현

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -27,17 +27,16 @@ public class StringCalculator {
             // trim()사용해 공백을 허용해준다.
             String cleanIntegers = integer.trim();
 
+            // 피연산자가 비어있을 경우, 예외 처리를 한다.
             if(cleanIntegers.isEmpty()) throw new IllegalArgumentException("빈 피연산자는 허용되지 않습니다.");
 
             int number;
 
-            try {
-                number = Integer.parseInt(cleanIntegers);
-                // 피연산자 위치에 숫자(양수)외에 문자가 올 경우, 예외 처리를 한다.
-            } catch (NumberFormatException e) {
-                throw new IllegalArgumentException("피연산자는 숫자(양수)만 입력 가능합니다." + cleanIntegers, e);
-            }
+            try { number = Integer.parseInt(cleanIntegers); }
+            // 피연산자 위치에 숫자(양수)외에 문자가 올 경우, 예외 처리를 한다.
+            catch (NumberFormatException e) { throw new IllegalArgumentException("피연산자는 숫자(양수)만 입력 가능합니다." + cleanIntegers, e); }
 
+            // 피연산자 양수 X → 예외처리
             if (number > 0) sum += number;
             else throw new IllegalArgumentException("피연산자는 양수만 가능합니다." + number);
         }
@@ -48,7 +47,40 @@ public class StringCalculator {
     // ex) //;\n1;2;3
     // 커스텀구분자 덧셈
     private static int addCustomDelimiter(String input){
-        return 0;
+
+        // 커스텀 구분자 추출
+        Character delimiter = input.charAt(2);
+        if(delimiter == ',' ||  delimiter == ':') throw new IllegalArgumentException("기본구분자(쉼표 또는 콜론)외에 다른 문자를 사용해 주세요.");
+
+        if(!(input.charAt(3) == '\\' && input.charAt(4) == 'n')) throw new IllegalArgumentException("잘못된 형식의 입력값입니다.");
+
+        // 본문(피연산자 + 연산자) 추출 (trim() → 공백 허용)
+        String body = input.substring(5).trim();
+
+
+        // 커스텀 구분자로 숫자 쪼갬
+        String[] integers = body.split(String.valueOf(delimiter));
+
+        int sum = 0;
+        for (String integer : integers) {
+            // trim()사용해 공백을 허용해준다.
+            String cleanIntegers = integer.trim();
+
+            // 피연산자가 비어있을 경우, 예외 처리를 한다.
+            if(cleanIntegers.isEmpty()) throw new IllegalArgumentException("빈 피연산자는 허용되지 않습니다.");
+
+            int number;
+
+            try { number = Integer.parseInt(cleanIntegers); }
+            // 피연산자 위치에 숫자(양수)외에 문자가 올 경우, 예외 처리를 한다.
+            catch (NumberFormatException e) { throw new IllegalArgumentException("피연산자는 숫자(양수)만 입력 가능합니다." + cleanIntegers, e); }
+
+            // 피연산자 양수 X → 예외처리
+            if (number > 0) sum += number;
+            else throw new IllegalArgumentException("피연산자는 양수만 가능합니다." + number);
+        }
+
+        return sum;
     }
 
 }
