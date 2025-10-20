c1838802a1b93e9d781d7acffc8ac87d428b6e25
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-10-20 16:37:56 +0900

    fix(calculator): 기본구분자덧셈 피연산자의  공백 허용 및 비숫자 입력 IllegalArgumentException으로  에러 처리

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -13,7 +13,7 @@ public class StringCalculator {
         if(Character.isDigit(firstWord)) return addBasicDelimiter(input);
         // 첫문자가 문자(//)로 시작하는지?
         else if(input.startsWith("//")) return addCustomDelimiter(input);
-        else throw new IllegalArgumentException("잘못된 형식의 입력값입니다.");
+        else throw new IllegalArgumentException("잘못된 형식의 입력값입니다." + input);
     }
 
     // ex) 1,2 / 1,2,3 / 1,2:3
@@ -23,11 +23,23 @@ public class StringCalculator {
         String[] integers = input.split(",|:");
 
         int sum = 0;
-        for(int i=0; i< integers.length; i++){
-            int number = Integer.parseInt(integers[i]);
-            
-            if(number > 0) sum += number;
-            else throw new IllegalArgumentException("피연산자는 양수만 가능합니다.");
+        for (String integer : integers) {
+            // trim()사용해 공백을 허용해준다.
+            String cleanIntegers = integer.trim();
+
+            if(cleanIntegers.isEmpty()) throw new IllegalArgumentException("빈 피연산자는 허용되지 않습니다.");
+
+            int number;
+
+            try {
+                number = Integer.parseInt(cleanIntegers);
+                // 피연산자 위치에 숫자(양수)외에 문자가 올 경우, 예외 처리를 한다.
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException("피연산자는 숫자(양수)만 입력 가능합니다." + cleanIntegers, e);
+            }
+
+            if (number > 0) sum += number;
+            else throw new IllegalArgumentException("피연산자는 양수만 가능합니다." + number);
         }
 
         return sum;
