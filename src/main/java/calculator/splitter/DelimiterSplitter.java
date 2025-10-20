af79dfd8b5b365816f77595ef5c48b9d5ee54c7b
hyeonjichoi21 <ch5i_hj15@naver.com>
2025-10-20 22:53:24 +0900

    refactor(calculator): 커스텀 구분자 특수 문자 허용 및 개행 처리 방식 추가

--- src/main/java/calculator/splitter/DelimiterSplitter.java
+++ src/main/java/calculator/splitter/DelimiterSplitter.java
@@ -1,5 +1,7 @@
 package calculator.splitter;
 
+import java.util.regex.Pattern;
+
 public final class DelimiterSplitter {
     private DelimiterSplitter(){}
 
@@ -18,13 +20,18 @@ public final class DelimiterSplitter {
         char delimiter = input.charAt(2);
         if(delimiter == ',' ||  delimiter == ':') throw new IllegalArgumentException("기본구분자(쉼표 또는 콜론)외에 다른 문자를 사용해 주세요.");
 
-        if(!(input.charAt(3) == '\\' && input.charAt(4) == 'n')) throw new IllegalArgumentException("잘못된 형식의 입력값입니다.");
+        // 개행 표기 두 가지 모두 허용
+        int bodyStart;
+        if (input.charAt(3) == '\n') bodyStart =4;
+        else if (input.charAt(3) == '\\' && input.charAt(4) == 'n') bodyStart =5;
+        else throw new IllegalArgumentException("잘못된 형식의 입력값입니다.");
 
         // 본문(피연산자 + 연산자) 추출 (trim() → 공백 허용)
-        String body = input.substring(5).trim();
+        String body = input.substring(bodyStart).trim();
 
-
-        return body.split(String.valueOf(delimiter));
+        // 구분자 특수문자 허용
+        String regex = Pattern.quote(String.valueOf(delimiter));
+        return body.split(regex);
     }
 
 
